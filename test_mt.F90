26d379b2ea9d91e1304ff936c24a53ec0569bd75
Holger Wolff <holger.wolff@monash.edu>
2017-11-22 16:36:51 +1100

    converted test_mt to read and compare

--- test_mt.F90
+++ test_mt.F90
@@ -2,17 +2,23 @@ program test_mt
     use mt19937_mod, only: MT19937_RANDOM_SEED, MT19937_RANDOM_NUMBER
     USE iso_c_binding, ONLY : int32 => c_int32_t, real32 => c_float, real64 => c_double
     implicit none
-    real(kind=real64) :: r(10000)
+    real(kind=real64) :: r(10000), r2(10000)
     integer :: u, ios
 
-    open(newunit=u, file='data.txt', action='write', iostat=ios, &
-        status='unknown')
+    open(newunit=u, file='data.txt', action='read', iostat=ios, &
+        status='old')
     if (ios /= 0) stop "FAILED TO OPEN data.txt"
 
     CALL MT19937_RANDOM_SEED([1_int32])
     CALL MT19937_RANDOM_NUMBER(r)
 
-    write(u, '(F8.5)') r
+    read(u, '(F8.5)') r2
+
+    if ( all(abs(r - r2) < 0.0001) ) then
+        print*, "all identical"
+    else
+        print*, "Failures"
+    end if
 
     close(u)
 
